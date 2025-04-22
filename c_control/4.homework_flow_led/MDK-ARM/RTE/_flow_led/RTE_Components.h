56626990e6b66fc6a6053b25fad508f7271f6132
shyyyyyly <syfyg205ywy4@163.com>
2025-04-22 17:38:08 +0800

    新增c_control

---
+++ c_control/4.homework_flow_led/MDK-ARM/RTE/_flow_led/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'flow_led' 
+ * Target:  'flow_led' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
